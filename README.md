1f01c7a6d722ca2aaaf8f9bbf8a140df15b96f80
Krzysztof Chojnowski <krzysiek@embedev.com>
2016-09-28 21:47:21 +0200

    driver changed to work with ODROID Ubuntu14; removed scale and vref

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # mcp3021_linux_driver
 drivers/iio/adc/mcp3021.c
-include/linux/iio/adc/mcp3021.h
--- mcp3021.c
+++ mcp3021.c
@@ -19,14 +19,11 @@
 #include <linux/i2c.h>
 #include <linux/err.h>
 #include <linux/iio/iio.h>
-#include <linux/iio/adc/mcp3021.h>
 
 #define MCP3021_DRV_NAME "mcp3021"
 
 struct mcp3021_data{
 	struct i2c_client *client;
-	u16 vref_mv;
-
 };
 
 static const struct iio_chan_spec mcp3021_channel = {
@@ -34,7 +31,6 @@ static const struct iio_chan_spec mcp3021_channel = {
 	.indexed	= 1,
 	.channel	= 0,
 	.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
-	.info_mask_shared_by_type = BIT(IIO_CHAN_INFO_SCALE),
 };
 
 static int mcp3021_read_i2c_value(const struct i2c_client *client) {
@@ -53,17 +49,11 @@ static int mcp3021_read_raw(struct iio_dev *indio_dev,
 			   int *val, int *val2, long mask)
 {
 	struct mcp3021_data *data = iio_priv(indio_dev);
-	unsigned long scale_uv;
 
 	switch (mask) {
 	case IIO_CHAN_INFO_RAW:
 		*val = mcp3021_read_i2c_value(data->client);
 		return IIO_VAL_INT;
-	case IIO_CHAN_INFO_SCALE:
-		scale_uv = (data->vref_mv*1000) >> 10;
-		*val =  scale_uv / 1000000;
-		*val2 = scale_uv % 1000000;
-		return IIO_VAL_INT_PLUS_MICRO;
 	}
 	return -EINVAL;
 }
@@ -78,21 +68,14 @@ static int mcp3021_probe(struct i2c_client *client,
 {
 	struct mcp3021_data *data;
 	struct iio_dev *indio_dev;
-	struct mcp3021_platform_data *platform_data = client->dev.platform_data;
 	int err;
 
-	if (!platform_data) {
-		dev_err(&client->dev, "invalid platform data");
-		return -EINVAL;
-	}
-
-	indio_dev = devm_iio_device_alloc(&client->dev, sizeof(*data));
+	indio_dev = iio_device_alloc(sizeof(*data));
 	if (indio_dev == NULL)
 		return -ENOMEM;
 	data = iio_priv(indio_dev);
 	i2c_set_clientdata(client, indio_dev);
 	data->client = client;
-	data->vref_mv = platform_data->vref_mv; 
 
 	indio_dev->dev.parent = &client->dev;
 	indio_dev->info = &mcp3021_info;
